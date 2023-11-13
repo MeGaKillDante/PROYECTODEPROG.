9dc9485cea12fa7736d7f31da78a3abb58508a19
artur <artur@Arturo>
2023-11-12 22:40:39 -0500

    YA ESTA LITO2e2

---
+++ src/main/java/com/mycompany/pruebagitparaproyecto/PRUEBAGITPARAPROYECTO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebagitparaproyecto;
+
+/**
+ *
+ * @author artur
+ */
+public class PRUEBAGITPARAPROYECTO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("22543674");
+    }
+}
